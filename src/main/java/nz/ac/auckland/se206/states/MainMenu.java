13362166295da88be60039c44d955905e19de48f
Patrick <sheng.patrick@gmail.com>
2024-09-14 14:02:26 +1200

    Exit button in main menu now can properly close application

--- src/main/java/nz/ac/auckland/se206/states/MainMenu.java
+++ src/main/java/nz/ac/auckland/se206/states/MainMenu.java
@@ -1,6 +1,8 @@
 package nz.ac.auckland.se206.states;
 
 import java.io.IOException;
+
+import javafx.application.Platform;
 import javafx.scene.input.MouseEvent;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameStateContext;
@@ -19,7 +21,7 @@ public class MainMenu implements GameState {
         System.out.println("Settings clicked");
         break;
       case "rectExit":
-        System.out.println("Exit clicked");
+        Platform.exit();
         break;
     }
   }
