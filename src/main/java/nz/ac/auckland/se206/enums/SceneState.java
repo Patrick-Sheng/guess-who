94cdced2bb170afccd839f482ef06bc481b6125a
FeroxFoxxo <feroxfoxxo@gmail.com>
2024-09-16 05:16:14 +1200

    feat: added images for title screen, settings page, manor, all characters and game over screens.

--- src/main/java/nz/ac/auckland/se206/enums/SceneState.java
+++ src/main/java/nz/ac/auckland/se206/enums/SceneState.java
@@ -1,8 +1,8 @@
 package nz.ac.auckland.se206.enums;
 
 public enum SceneState {
-  MAIN_MENU(),
-  START_GAME(),
-  START_GUESSING(),
-  END_GAME();
+  MAIN_MENU,
+  START_GAME,
+  START_GUESSING,
+  END_GAME
 }
