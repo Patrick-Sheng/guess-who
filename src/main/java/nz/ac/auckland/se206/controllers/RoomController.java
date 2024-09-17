55e6d51626c9fedc1dfbcf648db590f3344efaa7
Eason Chang <echa931@aucklanduni.ac.nz>
2024-09-17 15:12:22 +1200

    feat: add hovering effect to the room on the map

--- src/main/java/nz/ac/auckland/se206/controllers/RoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/RoomController.java
@@ -1,7 +1,11 @@
 package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.enums.SceneState;
 
@@ -11,8 +15,11 @@ import nz.ac.auckland.se206.enums.SceneState;
  */
 public class RoomController {
 
+  @FXML private ImageView exitImage;
   @FXML private Pane paneRoom;
   @FXML private Pane paneMap;
+  @FXML private GridPane gridMap;
+  @FXML private Text textMap;
 
   @FXML
   public void initialize() {
@@ -34,5 +41,41 @@ public class RoomController {
   private void onExitMap() {
     paneMap.setVisible(false);
     paneRoom.setOpacity(1);
+  }
+
+  @FXML
+  private void enterMap(MouseEvent event) {
+    System.out.println("enter");
+
+    Pane hoverPane = (Pane) event.getSource();
+    String paneId = hoverPane.getId();
+    String text = "";
+
+    switch (paneId) {
+      case "auntieRoom":
+        text = "Auntie Room";
+        break;
+      case "childRoom":
+        text = "Child Room";
+        break;
+      case "gardenerRoom":
+        text = "GardenerRoom";
+        break;
+      case "mainRoom":
+        text = "Crime scene";
+        break;
+      default:
+        text = "";
+    }
+
+    textMap.setText(text);
+    exitImage.setVisible(false);
+  }
+
+  @FXML
+  private void exitMap(MouseEvent event) {
+    System.out.println("exit");
+    textMap.setText("");
+    exitImage.setVisible(true);
   }
 }
