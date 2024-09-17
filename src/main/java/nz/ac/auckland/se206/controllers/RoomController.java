ff88010c7b46076bd61e133157f037ca3f975ede
Eason Chang <echa931@aucklanduni.ac.nz>
2024-09-17 12:59:13 +1200

    feat: added button to load the map

--- src/main/java/nz/ac/auckland/se206/controllers/RoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/RoomController.java
@@ -1,9 +1,38 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import nz.ac.auckland.se206.App;
+import nz.ac.auckland.se206.enums.SceneState;
+
 /**
  * Controller class for the room view. Handles user interactions within the room where the user can
  * chat with customers and guess their profession.
  */
 public class RoomController {
 
+  @FXML private Pane paneRoom;
+  @FXML private Pane paneMap;
+
+  @FXML
+  public void initialize() {
+    paneMap.setVisible(false);
+  }
+
+  @FXML
+  private void onGuess() {
+    App.setRoot(SceneState.START_GUESSING);
+  }
+
+  @FXML
+  private void onMap() {
+    paneMap.setVisible(true);
+    paneRoom.setOpacity(0.5);
+  }
+
+  @FXML
+  private void onExitMap() {
+    paneMap.setVisible(false);
+    paneRoom.setOpacity(1);
+  }
 }
